d3df6ff5d49bf56d5e2f677caa5beaf5b728cbad
Abel Borit <abelboritguitton@gmail.com>
2024-11-20 11:54:06 -0500

    [add] index assets barrel

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,165 @@
+/* PNG */
+// import hero_img from "./png/hero_img.png";
+import logo_img from "./png/logo_img.png";
+// import p_img1 from "./png/p_img1.png";
+// import p_img2_1 from "./png/p_img2_1.png";
+// import p_img2_2 from "./png/p_img2_2.png";
+// import p_img2_3 from "./png/p_img2_3.png";
+// import p_img2_4 from "./png/p_img2_4.png";
+// import p_img3 from "./png/p_img3.png";
+// import p_img4_1 from "./png/p_img4_1.png";
+// import p_img4_2 from "./png/p_img4_2.png";
+// import p_img4_3 from "./png/p_img4_3.png";
+// import p_img4_4 from "./png/p_img4_4.png";
+// import p_img5 from "./png/p_img5.png";
+// import p_img6 from "./png/p_img6.png";
+// import p_img7 from "./png/p_img7.png";
+// import p_img8 from "./png/p_img8.png";
+// import p_img9 from "./png/p_img9.png";
+// import p_img10 from "./png/p_img10.png";
+// import p_img11 from "./png/p_img11.png";
+// import p_img12 from "./png/p_img12.png";
+// import p_img13 from "./png/p_img13.png";
+// import p_img14 from "./png/p_img14.png";
+// import p_img15 from "./png/p_img15.png";
+// import p_img16 from "./png/p_img16.png";
+// import p_img17 from "./png/p_img17.png";
+// import p_img18 from "./png/p_img18.png";
+// import p_img19 from "./png/p_img19.png";
+// import p_img20 from "./png/p_img20.png";
+// import p_img21 from "./png/p_img21.png";
+// import p_img22 from "./png/p_img22.png";
+// import p_img23 from "./png/p_img23.png";
+// import p_img24 from "./png/p_img24.png";
+// import p_img25 from "./png/p_img25.png";
+// import p_img26 from "./png/p_img26.png";
+// import p_img28 from "./png/p_img28.png";
+// import p_img29 from "./png/p_img29.png";
+// import p_img31 from "./png/p_img31.png";
+// import p_img34 from "./png/p_img34.png";
+// import p_img35 from "./png/p_img35.png";
+// import p_img36 from "./png/p_img36.png";
+// import p_img39 from "./png/p_img39.png";
+// import p_img40 from "./png/p_img40.png";
+// import p_img41 from "./png/p_img41.png";
+// import p_img43 from "./png/p_img43.png";
+// import p_img44 from "./png/p_img44.png";
+// import p_img45 from "./png/p_img45.png";
+// import p_img46 from "./png/p_img46.png";
+// import p_img47 from "./png/p_img47.png";
+// import p_img48 from "./png/p_img48.png";
+// import p_img49 from "./png/p_img49.png";
+// import p_img50 from "./png/p_img50.png";
+// import p_img51 from "./png/p_img51.png";
+// import p_img52 from "./png/p_img52.png";
+
+/* WEBP */
+import hero_img from "./webp/hero_img.webp";
+// import logo_img from "./webp/logo_img.webp";
+import p_img1 from "./webp/p_img1.webp";
+import p_img2_1 from "./webp/p_img2_1.webp";
+import p_img2_2 from "./webp/p_img2_2.webp";
+import p_img2_3 from "./webp/p_img2_3.webp";
+import p_img2_4 from "./webp/p_img2_4.webp";
+import p_img3 from "./webp/p_img3.webp";
+import p_img4_1 from "./webp/p_img4_1.webp";
+import p_img4_2 from "./webp/p_img4_2.webp";
+import p_img4_3 from "./webp/p_img4_3.webp";
+import p_img4_4 from "./webp/p_img4_4.webp";
+import p_img5 from "./webp/p_img5.webp";
+import p_img6 from "./webp/p_img6.webp";
+import p_img7 from "./webp/p_img7.webp";
+import p_img8 from "./webp/p_img8.webp";
+import p_img9 from "./webp/p_img9.webp";
+import p_img10 from "./webp/p_img10.webp";
+import p_img11 from "./webp/p_img11.webp";
+import p_img12 from "./webp/p_img12.webp";
+import p_img13 from "./webp/p_img13.webp";
+import p_img14 from "./webp/p_img14.webp";
+import p_img15 from "./webp/p_img15.webp";
+import p_img16 from "./webp/p_img16.webp";
+import p_img17 from "./webp/p_img17.webp";
+import p_img18 from "./webp/p_img18.webp";
+import p_img19 from "./webp/p_img19.webp";
+import p_img20 from "./webp/p_img20.webp";
+import p_img21 from "./webp/p_img21.webp";
+import p_img22 from "./webp/p_img22.webp";
+import p_img23 from "./webp/p_img23.webp";
+import p_img24 from "./webp/p_img24.webp";
+import p_img25 from "./webp/p_img25.webp";
+import p_img26 from "./webp/p_img26.webp";
+import p_img28 from "./webp/p_img28.webp";
+import p_img29 from "./webp/p_img29.webp";
+import p_img31 from "./webp/p_img31.webp";
+import p_img34 from "./webp/p_img34.webp";
+import p_img35 from "./webp/p_img35.webp";
+import p_img36 from "./webp/p_img36.webp";
+import p_img39 from "./webp/p_img39.webp";
+import p_img40 from "./webp/p_img40.webp";
+import p_img41 from "./webp/p_img41.webp";
+import p_img43 from "./webp/p_img43.webp";
+import p_img44 from "./webp/p_img44.webp";
+import p_img45 from "./webp/p_img45.webp";
+import p_img46 from "./webp/p_img46.webp";
+import p_img47 from "./webp/p_img47.webp";
+import p_img48 from "./webp/p_img48.webp";
+import p_img49 from "./webp/p_img49.webp";
+import p_img50 from "./webp/p_img50.webp";
+import p_img51 from "./webp/p_img51.webp";
+import p_img52 from "./webp/p_img52.webp";
+
+export {
+  hero_img,
+  logo_img,
+  p_img1,
+  p_img2_1,
+  p_img2_2,
+  p_img2_3,
+  p_img2_4,
+  p_img3,
+  p_img4_1,
+  p_img4_2,
+  p_img4_3,
+  p_img4_4,
+  p_img5,
+  p_img6,
+  p_img7,
+  p_img8,
+  p_img9,
+  p_img10,
+  p_img11,
+  p_img12,
+  p_img13,
+  p_img14,
+  p_img15,
+  p_img16,
+  p_img17,
+  p_img18,
+  p_img19,
+  p_img20,
+  p_img21,
+  p_img22,
+  p_img23,
+  p_img24,
+  p_img25,
+  p_img26,
+  p_img28,
+  p_img29,
+  p_img31,
+  p_img34,
+  p_img35,
+  p_img36,
+  p_img39,
+  p_img40,
+  p_img41,
+  p_img43,
+  p_img44,
+  p_img45,
+  p_img46,
+  p_img47,
+  p_img48,
+  p_img49,
+  p_img50,
+  p_img51,
+  p_img52,
+};
