2f31f183aa7f130c9310240093174916e9894f60
Abel Borit <abelboritguitton@gmail.com>
2024-11-25 18:00:06 -0500

    [add] ProductInterface

---
+++ frontend/database_local/products.interface.ts
@@ -0,0 +1,12 @@
+export interface ProductInterface {
+  _id: number;
+  name: string;
+  description: string;
+  price: string;
+  image: string[];
+  category: string;
+  subCategory: string;
+  sizes: string[];
+  date: number;
+  bestseller: boolean;
+}
