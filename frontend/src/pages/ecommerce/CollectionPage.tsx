45cd17df26af07161b60565a503b2c3207ae990f
Abel Borit <abelboritguitton@gmail.com>
2024-12-04 18:02:13 -0500

    [style] CollectionPage

--- frontend/src/pages/ecommerce/CollectionPage.tsx
+++ frontend/src/pages/ecommerce/CollectionPage.tsx
@@ -28,7 +28,7 @@ export const CollectionPage = () => {
   });
 
   return (
-    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+    <div className="flex flex-col items-start justify-center w-full sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
       {/* Filter Options */}
       <FilterWrapper
         categoryFilter={categoryFilter}
