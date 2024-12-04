55a3ef323e217c323fbb0b9a2d42d0879ab9df59
Abel Borit <abelboritguitton@gmail.com>
2024-12-04 17:48:43 -0500

    [style] Footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { logo_img } from "../../assets";
 export const Footer = () => {
   return (
     <div>
-      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-20 text-sm">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-12 text-sm">
         <div>
           <img src={logo_img} alt="logo" className="mb-5 w-32" />
           <p className="w-full md:w-2/3 text-gray-600">
