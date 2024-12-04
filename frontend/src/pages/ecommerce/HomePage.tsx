55c76542ba5fcb22216c1559b8022bc9e07c78eb
Abel Borit <abelboritguitton@gmail.com>
2024-12-04 18:00:23 -0500

    [style] HomePage

--- frontend/src/pages/ecommerce/HomePage.tsx
+++ frontend/src/pages/ecommerce/HomePage.tsx
@@ -6,7 +6,7 @@ import { OurPolicy } from "../../components/OurPolicy";
 
 export const HomePage = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center w-full">
       <HeroComponent />
 
       <LatestCollection />
