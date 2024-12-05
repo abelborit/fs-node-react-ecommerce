1645c009979006f2e8a4ac4285840c656d6b89a9
Abel Borit <abelboritguitton@gmail.com>
2024-12-05 18:33:52 -0500

    [update] fakeRatingProductAndComments

--- frontend/src/pages/product/fakeRatingProductAndComments.ts
+++ frontend/src/pages/product/fakeRatingProductAndComments.ts
@@ -1,37 +1,58 @@
 export const fakeRatingProductAndComments = [
   {
     rating: 0,
-    comments:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, necessitatibus!",
+    user: {
+      name: "Alice Bart",
+      comments:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, necessitatibus!",
+    },
   },
   {
     rating: 4,
-    comments:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus et optio magni perferendis nesciunt fugiat doloremque ab hic, consectetur consequuntur.",
+    user: {
+      name: "Bob Class",
+      comments:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus et optio magni perferendis nesciunt fugiat doloremque ab hic, consectetur consequuntur.",
+    },
   },
   {
     rating: 2,
-    comments:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, necessitatibus!",
+    user: {
+      name: "Charlie Wok",
+      comments:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, necessitatibus!",
+    },
   },
   {
     rating: 1,
-    comments:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus et optio magni perferendis nesciunt fugiat doloremque ab hic, consectetur consequuntur.",
+    user: {
+      name: "Diana khan",
+      comments:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus et optio magni perferendis nesciunt fugiat doloremque ab hic, consectetur consequuntur.",
+    },
   },
   {
     rating: 5,
-    comments:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt beatae dolor esse eos officia harum obcaecati culpa eligendi molestias unde sit quidem consequatur ipsa possimus, quod, quis enim. Rem, expedita.",
+    user: {
+      name: "Eve Lenovy",
+      comments:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt beatae dolor esse eos officia harum obcaecati culpa eligendi molestias unde sit quidem consequatur ipsa possimus, quod, quis enim. Rem, expedita.",
+    },
   },
   {
     rating: 5,
-    comments:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, necessitatibus!",
+    user: {
+      name: "Frank Chore",
+      comments:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, necessitatibus!",
+    },
   },
   {
     rating: 2,
-    comments:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus et optio magni perferendis nesciunt fugiat doloremque ab hic, consectetur consequuntur.",
+    user: {
+      name: "Grace Compett",
+      comments:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus et optio magni perferendis nesciunt fugiat doloremque ab hic, consectetur consequuntur.",
+    },
   },
 ];
