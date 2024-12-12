8d798698306ea793a2904dd5fe71051bd79c5d49
Abel Borit <abelboritguitton@gmail.com>
2024-12-12 18:26:05 -0500

    [update] validationSchemaRules

--- frontend/src/components/DeliveryInformationForm/validationSchemaRules.ts
+++ frontend/src/components/DeliveryInformationForm/validationSchemaRules.ts
@@ -1,15 +1,23 @@
 import * as Yup from "yup";
 
+/*
+/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/
+
+  - a-zA-Z: Permite letras minúsculas y mayúsculas.
+  - áéíóúÁÉÍÓÚñÑ: Soporte para caracteres con tilde y la ñ.
+  - \s: Permite espacios.
+*/
+
 /* se coloca afuera para evitar que se cree de nuevo innecesariamente y almacenar espacio en cada renderizado */
 export const validationSchemaRules = Yup.object({
   /* colocar las reglas para los inputs */
   firstName: Yup.string()
-    .matches(/^[a-zA-Z]+$/, "Must contain only letters")
+    .matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, "Must contain only letters")
     .min(2, "Must be 2 characters or more")
     .max(15, "Must be 15 characters or less")
     .required("This input is required"),
   lastName: Yup.string()
-    .matches(/^[a-zA-Z]+$/, "Must contain only letters")
+    .matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, "Must contain only letters")
     .min(2, "Must be 2 characters or more")
     .max(15, "Must be 15 characters or less")
     .required("This input is required"),
@@ -21,12 +29,12 @@ export const validationSchemaRules = Yup.object({
     .max(50, "Must be 50 characters or less")
     .required("This input is required"),
   city: Yup.string()
-    .matches(/^[a-zA-Z]+$/, "Must contain only letters")
+    .matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, "Must contain only letters")
     .min(2, "Must be 2 characters or more")
     .max(20, "Must be 20 characters or less")
     .required("This input is required"),
   state: Yup.string()
-    .matches(/^[a-zA-Z]+$/, "Must contain only letters")
+    .matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, "Must contain only letters")
     .min(2, "Must be 2 characters or more")
     .max(20, "Must be 20 characters or less")
     .required("This input is required"),
@@ -36,13 +44,13 @@ export const validationSchemaRules = Yup.object({
     .max(10, "Must be 10 characters or less")
     .required("This input is required"),
   country: Yup.string()
-    .matches(/^[a-zA-Z]+$/, "Must contain only letters")
+    .matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, "Must contain only letters")
     .min(2, "Must be 2 characters or more")
     .max(20, "Must be 20 characters or less")
     .required("This input is required"),
   phone: Yup.string()
     .matches(/^\d+$/, "Must contain only numbers")
-    .min(10, "Must be 10 characters or more")
-    .max(15, "Must be 15 characters or less")
+    .min(9, "Must be 9 characters")
+    .max(9, "Must be 9 characters")
     .required("This input is required"),
 });
