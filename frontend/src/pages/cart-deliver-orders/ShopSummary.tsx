1a2f3708f66d91f24e2abcc9f1cb9520142bf21e
Abel Borit <abelboritguitton@gmail.com>
2024-12-17 15:09:04 -0500

    [update] ShopSummary to use hook useBuyProductsLocalStorage

--- frontend/src/pages/cart-deliver-orders/ShopSummary.tsx
+++ frontend/src/pages/cart-deliver-orders/ShopSummary.tsx
@@ -4,26 +4,60 @@ import { ShopProductsSummary } from "../../components/ShopProductsSummary";
 import { TitleComponent } from "../../components/TitleComponent/index";
 import { useFormDeliveryContext } from "../../context/formDeliveryContext/FormDeliveryContext";
 import { useShopContext } from "../../context/shopContext/ShopContext";
+import {
+  useBuyProductsLocalStorage,
+  UseBuyProductsLocalStorageInterface,
+} from "../../hooks/useBuyProductsLocalStorage";
+import { useNavigate } from "react-router-dom";
 
 export const ShopSummary = () => {
+  const navigate = useNavigate();
   const { cartItems, currency, delivery_fee, handleGetCountAmount } =
     useShopContext();
   const { formDeliveryValues, selectedMethod } = useFormDeliveryContext();
+  const { addPurchase } = useBuyProductsLocalStorage();
+
+  const handleGoOrders = () => {
+    navigate("/orders");
+  };
 
   const handleBuyProducts = () => {
-    /* una vez finalizada la compra se podría eliminar todo el localStorage para que todo desde cero */
+    /* una vez finalizada la compra se podría eliminar todo el localStorage para que todo desde cero. También sería bueno enviar la fecha de la compra para que se pueda jalar después esa información desde el backend o sino que el backend pueda generar la fecha de compra al recibir esta solicitud */
     console.log("handleBuyProducts", {
       cartItems,
       handleGetCountAmount: handleGetCountAmount() + delivery_fee,
       formDeliveryValues,
       selectedMethod,
+      shopDate: new Date(),
     });
 
-    toast.success("Satisfactory purchase!", {
-      autoClose: 1500,
+    const newPurchase: UseBuyProductsLocalStorageInterface = {
+      cartItems,
+      formDeliveryValues,
+      getCountAmount: handleGetCountAmount() + delivery_fee,
+      selectedMethod,
+      shopDate: new Date(),
+      /* Sumar 1 día a la fecha actual */
+      // shopDate: (() => {
+      //   const currentDate = new Date();
+      //   currentDate.setDate(currentDate.getDate() + 1); // Suma un día
+      //   return currentDate;
+      // })(),
+    };
+
+    addPurchase(newPurchase);
+
+    toast.success("Processing your purchase!", {
+      autoClose: 2000,
       pauseOnHover: false,
       position: "bottom-right",
-      onClose: () => {}, // se podría enviar al home o a la ruta de las órdenes "orders" para hacer un seguimiento
+    });
+
+    toast.info("Redirecting to Orders Page", {
+      autoClose: 3000,
+      pauseOnHover: false,
+      position: "bottom-right",
+      onClose: handleGoOrders,
     });
   };
 
@@ -33,7 +67,7 @@ export const ShopSummary = () => {
         <TitleComponent firstText="SHOP" secondText="SUMMARY" />
       </div>
 
-      <div className="w-full flex flex-col gap-8 bg-gray-100 p-2 rounded-md">
+      <div className="w-full flex flex-col gap-6 sm:gap-8 bg-gray-100 p-2 rounded-md">
         <div className="flex flex-col gap-6 bg-white shadow rounded-md p-4">
           <ShopProductsSummary cartItems={cartItems} currency={currency} />
         </div>
