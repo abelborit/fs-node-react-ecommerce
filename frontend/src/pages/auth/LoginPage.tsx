49132dfc9b980ae6b57a501e2291bd4674f32e2b
Abel Borit <abelboritguitton@gmail.com>
2024-12-19 17:06:25 -0500

    [update] LoginPage & RegisterPage

--- frontend/src/pages/auth/LoginPage.tsx
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -1,3 +1,18 @@
+import { LoginForm } from "../../components/LoginForm";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="w-full flex flex-col items-center justify-center pt-14 pb-6">
+      <div className="inline-flex gap-2 items-center justify-center mb-4 w-full sm:max-w-[450px]">
+        <p className="prata-regular text-3xl text-gray-500">
+          Welcome{" "}
+          <span className="prata-regular text-gray-700 font-medium">Back</span>
+        </p>
+
+        <div className="w-10 md:w-12 h-[1px] sm:h-[2px] bg-gray-700"></div>
+      </div>
+
+      <LoginForm />
+    </div>
+  );
 };
