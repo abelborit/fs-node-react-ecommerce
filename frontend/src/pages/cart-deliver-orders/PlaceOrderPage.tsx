26a43a405bc349aa87aaf5dd36f640231737e7e5
Abel Borit <abelboritguitton@gmail.com>
2024-12-26 15:05:19 -0500

    [fix] PlaceOrderPage navigate to another page & [add] new validations

--- frontend/src/pages/cart-deliver-orders/PlaceOrderPage.tsx
+++ frontend/src/pages/cart-deliver-orders/PlaceOrderPage.tsx
@@ -4,13 +4,24 @@ import { TitleComponent } from "../../components/TitleComponent/index";
 import { PaymentMethods } from "../../components/PaymentMethods";
 import { useFormDeliveryContext } from "../../context/formDeliveryContext/FormDeliveryContext";
 import { useNavigate } from "react-router-dom";
+import { useFormUserContext } from "../../context/formUserContext/FormUserContext";
 
 export const PlaceOrderPage = () => {
   const navigate = useNavigate();
-  const { isFormValid, selectedMethod } = useFormDeliveryContext();
+  const { isFormValid, selectedMethod, formDeliveryValues } =
+    useFormDeliveryContext();
+  const { userInfo } = useFormUserContext();
+  const { isAuthenticated } = userInfo;
+
+  /* Verificar si todos los campos tienen valores no vacíos */
+  const isFieldsNotEmpty = Object.values(formDeliveryValues).every(
+    (value) => value.trim() !== "" // Evitar valores en blanco
+  );
 
   const handleGoShopSummary = () => {
-    navigate("/shop-summary");
+    if (isFormValid && selectedMethod && isAuthenticated) {
+      navigate("/shop-summary");
+    }
   };
 
   return (
@@ -31,9 +42,9 @@ export const PlaceOrderPage = () => {
             {/* Submit Button */}
             <div className="flex flex-col group items-end mt-2 justify-center w-[12.5rem]">
               <button
-                disabled={!isFormValid && !selectedMethod}
+                disabled={!isFormValid && !selectedMethod && !isAuthenticated}
                 className={`py-3 text-sm w-44 ${
-                  isFormValid && selectedMethod
+                  isFormValid && selectedMethod && isAuthenticated
                     ? "bg-gray-900 text-white hover:bg-gray-700"
                     : "bg-gray-300 text-gray-500 cursor-not-allowed"
                 }`}
@@ -49,10 +60,18 @@ export const PlaceOrderPage = () => {
                     : "opacity-0 group-hover:opacity-100"
                 }`}
               >
-                {!isFormValid
+                {!isFieldsNotEmpty
                   ? "Complete delivery information"
+                  : !isFormValid
+                  ? "Validate delivery information"
                   : "Select a paymet method"}
               </p>
+
+              {isAuthenticated ? null : (
+                <p className="text-red-500 text-center text-sm">
+                  You must login to view your shop summary
+                </p>
+              )}
             </div>
           </div>
         </div>
