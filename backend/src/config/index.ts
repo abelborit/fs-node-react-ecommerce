3faa18cfed8568095ee1e329edf6694dc3cf418a
Abel Borit <abelboritguitton@gmail.com>
2024-12-30 18:11:40 -0500

    [add] bcryptAdapter & JwtAdapter & regularExps

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,1 +1,4 @@
 export * from "./envs.plugin";
+export * from "./bcrypt.adapter";
+export * from "./jwt.adapter";
+export * from "./regular-exp";
---
+++ backend/src/config/regular-exp.ts
@@ -0,0 +1,4 @@
+export const regularExps = {
+  // email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+  email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // tal como está en el backend y frontend
+};
