370f42295b9b86f86631a5bc09a6f36a739a004b
Abel Borit <abelboritguitton@gmail.com>
2025-01-02 22:37:25 -0500

    [update] ProductRoutes to use MulterAdapter.uploadFields as middleware in "create-product"

--- backend/src/presentation/products/routes.ts
+++ backend/src/presentation/products/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { ProductController } from "./controller";
+import { MulterAdapter } from "../../config";
 
 export class ProductRoutes {
   /* aquí se utiliza static functions porque como no se hará inyección de dependencias entonces no sería necesario instsanciar la clase AppRoutes y solo se coloca directo. También se están usando el get function para tener otra forma de realizar esta función, se podría realizar sin ese get (son solo diferentes formas de hacerlo) */
@@ -16,7 +17,28 @@ export class ProductRoutes {
     router.delete("/delete-product/:id", productController.deleteProduct);
 
     router.get("/get-products", productController.getProducts);
-    router.post("/create-product", productController.createProduct);
+    router.post(
+      "/create-product",
+      [
+        MulterAdapter.uploadFields([
+          /* El método uploadFields acepta un arreglo donde se definen:
+          - name: El nombre de los campos de archivo que se esperan en el formulario.
+          - maxCount: El número máximo de archivos permitidos para cada campo.
+          */
+          /* En este caso, se están esperando hasta 4 archivos con los nombres image1, image2, image3, y image4. Cada uno puede tener como máximo 1 archivo. */
+          { name: "image1", maxCount: 1 },
+          { name: "image2", maxCount: 1 },
+          { name: "image3", maxCount: 1 },
+          { name: "image4", maxCount: 1 },
+        ]),
+      ], // si es un solo middleware se puede colocar directo MulterAdapter.uploadFields sin el arreglo.
+
+      /* aquí tener en cuenta que como en el controller está como una función flecha entonces aquí no sería necesario bindear, es decir, no es necesario colocar el -- .bind(......) --. Si en el controller estuviera como una función tradicional y no como un función flecha y encima aquí en ProductRoutes no se colocara el -- .bind(......) -- entonces aquí nos saldrá un error similar a "TypeError: Cannot read properties of undefined (reading 'productService')" debido a que el controlador "ProductController" no está recibiendo correctamente la instancia de "ProductService" en su constructor, por lo que "productService" es undefined */
+      /* En Express, cuando se pasa un método de clase directamente como middleware (productController.createProduct), el "this" del método puede perder el contexto de la instancia. Esto es debido a cómo JavaScript maneja el contexto de this, por lo tanto, es recomendable hacer bind al método. Entonces aplicamos el ".bind" para que el método createProduct mantenga el contexto de this */
+      /* NOTA: Para evitar el bind, una forma común es definir "createProduct" (el que viene de productController) como una función flecha en el controlador, ya que las funciones flecha mantienen el contexto de this de la clase sin necesidad de hacer bind explícito. Al definir "createProduct" como una función flecha, el valor de this se refiere automáticamente a la instancia de "ProductController" y se elimina la necesidad de hacer bind en el archivo de rutas */
+      productController.createProduct // usando función flecha
+      // productController.createProduct.bind(productController) // usando función tradicional
+    );
 
     /* FORMA 2: aquí se está mandando la función con sus parámetros que es una segunda forma de hacerlo en vez de la de arriba  */
     // router.get("/get-products", (request, response) =>
